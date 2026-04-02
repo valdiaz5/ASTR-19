fa5723f150e5eca637c26534f3db51aaf5082819
Valerie Diaz <vdiaz5@ucsc.edu>
2026-04-02 14:20:20 -0700

    Added journal 1 session 2 program

---
+++ session_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 13:55:16 2026
+
+@author: valeriediaz
+"""
+
+# sum of two floating point numbers
+x = 23.7
+y = 45.9
+
+result_1 = x + y
+
+print("x+y=", result_1)
+print(type(result_1))
+
+#difference between two integers 
+
+a = 72
+b = 12
+
+result_2 = a - b 
+
+print("a-b=", result_2)
+print(type(result_2))
+
+
+#product of a floating point number and an integer 
+
+c = 23.9
+d = 34
+
+result_3 = c*d
+
+print("c*d=", result_3)
+print(type(result_3))
