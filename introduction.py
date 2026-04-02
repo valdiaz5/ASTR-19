6aa6c2e69cbbeb24d4240e8f49b4a183129a2dae
Valerie Diaz <vdiaz5@ucsc.edu>
2026-04-02 13:39:43 -0700

    Add files via upload
    This program is for prompt 1 of coding journal 1

---
+++ introduction.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 17:43:01 2026
+
+@author: valeriediaz
+"""
+
+def PrintName():
+    #this function prints my name and pronouns 
+    print("Valerie Diaz she/her")
+    
+def PrintFilm():
+    #this functions prints my favorite film
+    print("My favortie film is The Perks of Being A Wallflower.")
+    
+def PrintFood():
+    #this function pronts my favorite food
+    print("My favorite food is Pad Thai.")
+    
+def main():
+    #this is the main function of the program 
+    PrintName()
+    PrintFilm()
+    PrintFood()
+    
+if __name__=="__main__":
+    #when we run the program this executes first 
+    main()
